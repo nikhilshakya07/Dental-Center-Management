c0282f61105c73b6705f7c2a5d239dadf3a053cc
nikhilshakya07 <nikhilshakya1308@gmail.com>
2025-07-06 03:27:43 +0530

    Added styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,9 @@ module.exports = {
   theme: {
     extend: {
       colors: {
+        border: "hsl(var(--border))",
+        background: "hsl(var(--background))",
+        foreground: "hsl(var(--foreground))",
         primary: {
           50: '#eff6ff',
           100: '#dbeafe',
@@ -16,8 +19,14 @@ module.exports = {
         secondary: {
           50: '#f8fafc',
           100: '#f1f5f9',
+          200: '#e2e8f0',
+          300: '#cbd5e1',
+          400: '#94a3b8',
           500: '#64748b',
           600: '#475569',
+          700: '#334155',
+          800: '#1e293b',
+          900: '#0f172a'
         }
       }
     },
